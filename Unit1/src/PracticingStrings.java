5555e322dc548eee7370cdb9a1518f20069f914d
Churro <charanvenkatchandran@gmail.com>
2022-09-06 09:27:31 -0500

    cool

--- Unit1/src/PracticingStrings.java
+++ Unit1/src/PracticingStrings.java
@@ -36,16 +36,28 @@ public class PracticingStrings {
         System.out.println("The new phrase is: " + replacedPhrase);
 
 
-        System.out.print("What substring do you want (put only a comma in between the number): ");
+        System.out.print("What substring do you want (you can put 1 or 2 digits in and use a comma in between the number): ");
         //Could have technically done this with substring
         final String whatSubstrings = input.nextLine();
         String[] substringsArray = whatSubstrings.split(",", 2);
         String substringOfPhrase;
         if (substringsArray.length >= 2) {
-            substringOfPhrase = phrase.substring(substringsArray[0], substringsArray[1]);
+            substringOfPhrase = phrase.substring(Integer.parseInt(substringsArray[0]), Integer.parseInt(substringsArray[1]));
         } else {
-            substringOfPhrase = phrase.substring(substringsArray[0]);
+            substringOfPhrase = phrase.substring(Integer.parseInt(substringsArray[0]));
         }
-        System.out.println("The new phrase is: " + );
+        System.out.println("The new phrase is: " + substringOfPhrase );
+
+        final String lowerCase = phrase.toLowerCase();
+        System.out.println("Lowercase:" + lowerCase);
+        final String upperCase = phrase.toUpperCase();
+        System.out.println("Uppercase:" + upperCase);
+
+        System.out.print("What is your name (put it in this order first, last with the comma):");
+        final String name = input.nextLine();
+        final int indexOfComma = name.indexOf(",");
+        final String firstName = name.substring(0,indexOfComma);
+        final String lastName = name.substring(indexOfComma);
+        System.out.println("Your name backwards: " + lastName + ", " + firstName);
     }
 }
