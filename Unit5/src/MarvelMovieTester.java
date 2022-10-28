240fac00a698f3475b105f0f1bcdf2ddb888f4f0
Charan Venkat Chandran <chandran7824@students.d211.org>
2022-10-28 08:07:43 -0500

    Marvel Movie

--- Unit5/src/MarvelMovieTester.java
+++ Unit5/src/MarvelMovieTester.java
@@ -1,2 +1,28 @@
 public class MarvelMovieTester {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * This method will compare the strengths of their Power attribute, and then depending on who has
+     * more strength at the time of the battle, the winner is determined and both fighters will have their
+     * strength deducted accordingly (even the winner will take some damage).
+     * @param hero This is a SuperHero object representing the hero.
+     * @param villain This is a Villain object representing the villain.
+     */
+    public static void battle(SuperHero hero, Villain villain) {
+        Power heroPower = hero.getSuperPower();
+        Power villainPower = villain.getSuperPower();
+    }
+
+    /**
+     * This method will accept a SuperHero object, a Villain object, and an int num
+     * telling us how many times these two fighters will battle. This method will call the battle
+     * method num times and as long as each fighter still has strength, a battle will ensue.
+     * @param hero This is a SuperHero object representing the hero.
+     * @param villain This is a Villain object representing the villain.
+     */
+    public static void epicBattle(SuperHero hero, Villain villain) {
+
+    }
 }
--- Unit5/src/Villain.java
+++ Unit5/src/Villain.java
@@ -2,6 +2,11 @@ public class Villain {
     String name;
     Power superPower;
 
+    /**
+     * Creates a villain
+     * @param name name of villain
+     * @param superPower villains superpower
+     */
     Villain(String name, Power superPower) {
         this.name = name;
         this.superPower = superPower;
