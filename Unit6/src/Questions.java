a88701e882d225ce9b95c58bd5a06c3210c39c18
Charan Venkat Chandran <chandran7824@students.d211.org>
2022-12-06 09:30:30 -0600

    s

---
+++ Unit6/src/Questions.java
@@ -0,0 +1,42 @@
+public class Questions {
+    private String question, answer;
+    private int points;
+
+    public Questions(String question, String answer, int points) {
+        this.question = question;
+        this.answer = answer;
+        this.points = points;
+    }
+
+    /**
+     * Return the question with the points in parenthesis
+     * @return The question
+     */
+    public String toString() {
+        return this.question + " (" + this.points + ")";
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
