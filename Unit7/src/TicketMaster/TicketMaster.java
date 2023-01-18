8ca7b6508fba4c64f819fc820fd96a3191fd5eae
Churro <66274903+ChurroC@users.noreply.github.com>
2023-01-18 08:21:08 -0600

    wdaw
    da

--- Unit7/src/TicketMaster/TicketMaster.java
+++ Unit7/src/TicketMaster/TicketMaster.java
@@ -3,6 +3,7 @@ package TicketMaster;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicketMaster {
@@ -10,41 +11,34 @@ public class TicketMaster {
     final File myFile = new File("src/TicketMaster/showData.txt");
     final Scanner scan = new Scanner(myFile);
 
+
     public TicketMaster() throws FileNotFoundException {
         readShowData();
     }
 
     public void readShowData() {
         while (scan.hasNextLine()) {
-            //shows.add(new Show(
-            //        scan.next(),
-            //        scan.nextDouble(),
-            //        scan.nextInt(),
-            //        scan.next(),
-            //        scan.next()
-            //));
-
             final String date = scan.next();
-            System.out.println(date);
             final double price = scan.nextDouble();
-            System.out.println(price);
             final int qty = scan.nextInt();
-            System.out.println(qty);
-            final String performer = scan.next();
-            System.out.println(performer);
-            final String city = scan.next();
-            System.out.println(city);
+            final String[] restOfLine = scan.nextLine().split(", ");
+            final String performer = restOfLine[0].trim();
+            final String city = restOfLine[1].trim();
+
+            shows.add(new Show(date, price, qty, performer, city));
         }
     }
 
     @Override
     public String toString() {
-        String showData = """
-                Date\tPrice\tQty\t\tPerformer\tCity
-                ----------------------------------------    
-                """;
+        int lengthOfLongestPerformer = 0;
         for (Show show: shows) {
-            showData += show.getDate() + "\t" + show.getDate() + "\t" + show.getDate() + "\t" + show.getDate() + "\t" + show.getDate() + "\t";
+            if (show.getPerformer().length() > lengthOfLongestPerformer) lengthOfLongestPerformer = show.getPerformer().length();
+        }
+        String showData = "Date\t\tPrice\tQty\t\t" + String.format("%-" + (lengthOfLongestPerformer + 5) + "s", "Performer") + "City\n";
+        showData += new String(Arrays.fill(new char[showData.length()], '-'));
+        for (Show show: shows) {
+            showData += show.getDate() + "\t" + show.getPrice() + "\t" + show.getQty() + "\t\t" + String.format("%-" + (lengthOfLongestPerformer + 5) + "s", show.getPerformer()) + "" + show.getCity() + "\n";
         }
         return showData;
     }
