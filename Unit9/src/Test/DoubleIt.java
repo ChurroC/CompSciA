4144e53ef542d28e85dfda0e95de0696db18d5e0
Charan Venkat Chandran <chandran7824@students.d211.org>
2023-03-10 08:19:57 -0600

    zCDAADDA

--- Unit9/src/Test/DoubleIt.java
+++ Unit9/src/Test/DoubleIt.java
@@ -1,25 +1,11 @@
 package Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Field;
-import java.util.Objects;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@Repeatable(DoubleIts.class)
 public @interface DoubleIt {
-    private void checkIfSerializable(Object object) {
-        if (Objects.isNull(object)) {
-            System.out.println("The object is null");
-        }
-
-        Field clazz = object.getClass().getField();
-        if (!clazz.isAnnotationPresent(DoubleIt.class)) {
-            throw new JsonSerializationException("The class "
-                    + clazz.getSimpleName()
-                    + " is not annotated with JsonSerializable");
-        }
-    }
+    String test() default "test";
 }
+
