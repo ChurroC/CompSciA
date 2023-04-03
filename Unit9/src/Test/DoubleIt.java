023ee1f9cb9cfa1caa74417018a42e674143ab3d
Charan Venkat Chandran <chandran7824@students.d211.org>
2023-04-03 08:15:21 -0500

    awda
    wddwawdwda

--- Unit9/src/Test/DoubleIt.java
+++ Unit9/src/Test/DoubleIt.java
@@ -4,7 +4,7 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-@Repeatable(DoubleIts.class)
+//@Repeatable(DoubleIts.class)
 public @interface DoubleIt {
     String test() default "test";
 }
