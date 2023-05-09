da976583503ca8da052457ece0de633bdef4c607
Charan Venkat Chandran <chandran7824@students.d211.org>
2023-05-09 08:20:07 -0500

    Update Test.java

--- apReviews/src/Test.java
+++ apReviews/src/Test.java
@@ -1,12 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Test {
-    public static void main(String[] args) {
-        Integer val = 10;
-        int result1 = timesTwo(val);
-        Integer result2 = result1;
-        System.out.print(result2);
-    }
-    public static int timesTwo (int n)
-    {
-        return n * 2;
-    }
+        public static void main(String[] args) {
+            String myStr1 = "con";
+            String myStr2 = "cat";
+            System.out.println(myStr1.compareTo(myStr2)); // Returns 0 because they are equal
+        }
 }
